e965813866be62a1dfa7fc3658e2e748020bb1ae
visualholder <dxeracc@gmail.com>
2025-10-19 00:47:42 +0800

    Update plist.lua

--- plist.lua
+++ plist.lua
@@ -1,8 +1,5 @@
 return {
-    Premium = {},
-    PremiumBypass = {7270560546},
-    SpecialPerson = {9387422618}, 
+    Premium = {9387422618},
+    PremiumBypass = {},
+    SpecialPerson = {7270560546}, 
 }
-
-
-
